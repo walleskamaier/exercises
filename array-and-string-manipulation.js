0d7e06e67d09a6bba7f61d9c737b11c616e32199
Walleska Maier <walleskamaier@gmail.com>
2025-08-13 22:58:20 -0300

    add solution: implement basic array and string operations

---
+++ array-and-string-manipulation.js
@@ -0,0 +1,20 @@
+// Initialize an empty array named 'suitcase'
+let suitcase = []
+
+// TODO: Add items to your suitcase
+suitcase.push("t-shirt", "sandals", "bikini", "sunblock")
+
+// Declare a string variable for your goodbye message
+let message
+// TODO: Assign a farewell message to the variable
+message = "Goodbye, see you soon!"
+
+// Access the first and last item in your suitcase
+// TODO: Print the first item
+console.log(suitcase[0])
+// TODO: Print the last item
+console.log(suitcase[suitcase.length - 1])
+
+// Send off with a farewell in uppercase
+// TODO: Convert and print the farewell message in uppercase
+console.log(message.toUpperCase())
